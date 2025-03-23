52334f7ddd6b11c259c0b76319ec10e762a5dd3d
preetys <preeti.sharma@axonichealth.com>
2025-03-23 21:23:23 +0530

    expected output to be render

--- render_virtual_dom/src/App.jsx
+++ render_virtual_dom/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+/**
+ * Expected output 
+   
+<div class="heading-container">
+  This is
+  <h1 key="10" id="heading">devtools.tech</h1>
+  <h2 id="heading">is Awesome</h2>
+  <input type="text" value="Devtools tech"/>
+  <button>Click</button>
+  0
+  <div>React</div>
+  <div>Fragment</div>
+  ""
+</div>
+ */
